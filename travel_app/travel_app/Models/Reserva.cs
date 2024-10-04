9ab4f11291986c0e307ec3beb17f005911e55ff8
Srcox100 <lcox.esc@gmail.com>
2024-10-03 22:06:25 -0500

    avances

---
+++ travel_app/travel_app/Models/Reserva.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace travel_app.Models
+{
+    internal class Reserva
+    {
+    }
+}
---
+++ travel_app/travel_app/ViewModels/ReservaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace travel_app.ViewModels
+{
+    internal class ReservaViewModel
+    {
+    }
+}
