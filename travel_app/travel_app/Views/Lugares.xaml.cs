792217aa5d19ddb1e287cb478a9d1f8f229b0e4a
deisyllata <llatadeisy@gmail.com>
2024-10-11 00:58:37 -0500

    avances

--- travel_app/travel_app/Views/Lugares.xaml.cs
+++ travel_app/travel_app/Views/Lugares.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using travel_app.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +15,7 @@ namespace travel_app.Views
         public Lugares()
         {
             InitializeComponent();
-            Login.Clicked += async (sender, e) =>
+            boton.Clicked += async (sender, e) =>
             {
                 await Navigation.PushAsync(new HotelesPage());
             };
